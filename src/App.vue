3e08c523f5b683cead956a2f0d7152ca4aa11f13
林嘉伦 <linjialun@kingsware.cn>
2024-03-30 12:09:46 +0800

    3.30

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,35 @@ import HospitalFooter from '@/components/HospitalFooter.vue'
 </script>
 
 <template>
-  <div class="container">
-    <HospitalTitle /> 
-    <router-view />
-    <HospitalFooter />
-  </div>
+    <section class="main-title">
+      <HospitalTitle />
+    </section>
+    <section class="main-container">
+      <router-view />
+    </section>
+    <section class="main-footer">
+      <HospitalFooter />
+    </section>
 </template>
 
 <style scoped lang="scss">
-.container {
+#app {
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+
+  .main-title {
+    height: 70px;
+  }
+
+  .main-container {
+    min-height: calc(100% - 120px);
+  }
+  
+  .main-footer {
+    height: 50px;
+  }
 }
 </style>
--- src/components/HospitalFooter.vue
+++ src/components/HospitalFooter.vue
@@ -1,14 +1,23 @@
 <template>
-footer
+  <div class="hospital-footer">
+    <section></section>
+    <section></section>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue" 
+import { ref } from "vue"
 defineOptions({
   name: 'HospitalFooter'
 })
 </script>
 
 <style lang="scss" scoped>
-
+.hospital-footer {
+  padding: 0 59px;
+  background-color: #f0f2f5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
